ea1455635a2b4d4792a85c6a15b60b9ee2a50827
harrisontomasco <harrison.tomasco@mymail.champlain.edu>
2025-11-07 22:37:43 -0500

    Created Game Manager and start scene

---
+++ AllLawyersGoToHell/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    public void loadScene(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+}
