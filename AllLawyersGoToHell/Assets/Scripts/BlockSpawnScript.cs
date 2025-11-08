75fde4864833e56e4b6dbed507155126540e12c2
BoreJalex <amweiswasser@gmail.com>
2025-11-07 20:02:34 -0500

    Made some basics.

---
+++ AllLawyersGoToHell/Assets/Scripts/BlockSpawnScript.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawnScript : MonoBehaviour
+{
+	// Caching
+	[SerializeField] private Transform _spawnPoint;
+	[SerializeField] private GameObject _blockPrefab;
+	[SerializeField] private PlayerScript _pScript;
+
+	// Block Related Stuff
+	public List<GameObject> blockList = new List<GameObject>();
+	[SerializeField] private float _blockSpawnRate;
+
+	// Timer
+	private float _timeSinceSpawn;
+
+	// Block Stats
+	[SerializeField] private float _boxFallSpeed;
+
+	private void Update()
+	{
+		_timeSinceSpawn += Time.deltaTime * _blockSpawnRate;
+
+		if(_timeSinceSpawn >= 2)
+		{
+			_timeSinceSpawn = 0;
+			SpawnBlock();
+		}
+	}
+
+	void SpawnBlock()
+	{
+		GameObject block = Instantiate(_blockPrefab, _spawnPoint.position, Quaternion.identity);
+
+		if(blockList.Count == 0)
+		{
+			blockList.Add(block);
+			_pScript.currentBox = block;
+		}
+		else
+			blockList.Add(block);
+
+		block.AddComponent<BoxScript>();
+		block.GetComponent<BoxScript>().Initialize(_boxFallSpeed, _pScript);
+	}
+}
+
+public class BoxScript : MonoBehaviour
+{
+	// Cache
+	private float _fallSpeed;
+	private PlayerScript _pScript;
+	
+
+	public void Initialize(float fallSpeed, PlayerScript pScript)
+	{
+		_fallSpeed = fallSpeed;
+		_pScript = pScript;
+	}
+
+	private void Update()
+	{
+		transform.position = transform.position - new Vector3(0, _fallSpeed, 0) * Time.deltaTime;
+
+		if (transform.position.y <= -7)
+			Destroy(gameObject);
+	}
+}
---
+++ AllLawyersGoToHell/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+	// Caching
+	[SerializeField] private BlockSpawnScript _bScript;
+
+	// Trackers
+	public GameObject currentBox = null; 
+
+	private void Update()
+	{
+		
+	}
+}
