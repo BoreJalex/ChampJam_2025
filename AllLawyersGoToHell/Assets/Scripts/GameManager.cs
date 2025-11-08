c244ab06e08e58a2d38cf97b24943cc6825fed15
harrisontomasco <harrison.tomasco@mymail.champlain.edu>
2025-11-07 23:16:13 -0500

    UI Slider and options menu

--- AllLawyersGoToHell/Assets/Scripts/GameManager.cs
+++ AllLawyersGoToHell/Assets/Scripts/GameManager.cs
@@ -1,17 +1,24 @@
 using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
-
+    GameObject optionsScreen;
+    GameObject mainScreen;
+    private bool isOptionsOpen = false;
+    public float speedMultiplier;
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
+            optionsScreen = GameObject.Find("OptionsMenu");
+            mainScreen = GameObject.Find("StartScreen");
+            optionsScreen.SetActive(false);
             DontDestroyOnLoad(gameObject);
         }
         else
@@ -24,5 +31,33 @@ public class GameManager : MonoBehaviour
     public void loadScene(string sceneName)
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    public void toggleOptions()
+    {
+        if (isOptionsOpen)
+        {
+            // Close options menu
+            isOptionsOpen = false;
+            optionsScreen.SetActive(false);
+            mainScreen.SetActive(true);
+        }
+        else
+        {
+            // Open options menu
+            isOptionsOpen = true;
+            optionsScreen.SetActive(true);
+            mainScreen.SetActive(false);
+        }
+    }
+
+    public void ChangeSpeed(float speed)
+    {
+        speedMultiplier = speed;
     }
 }
